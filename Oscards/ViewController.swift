3a14437f5a5433771b72665936d6997a82a4111f
Silvio <silviocresci1996@gmail.com>
2019-11-15 16:19:48 +0100

    Add Layout

--- Oscards/ViewController.swift
+++ Oscards/ViewController.swift
@@ -40,4 +40,94 @@ class SecondViewController : UIViewController{
     @IBOutlet weak var nav: UINavigationItem!
 }
 
+class RulesViewController : UIViewController{
+override func viewDidLoad() {
+    
+        super.viewDidLoad()
+    }
+}
 
+class HomeViewController : UIViewController{
+override func viewDidLoad() {
+    
+        super.viewDidLoad()
+    }
+}
+
+class SelectViewController : UIViewController{
+
+    @IBOutlet weak var selectButton: UIButton!
+    @IBOutlet var nums: [UIButton]!
+    
+    var n : CGFloat = 0
+    var check : CGFloat = 0
+    
+    override func viewDidLoad() {
+    
+        super.viewDidLoad()
+    }
+    @IBAction func handleSelection(_ sender: UIButton) {
+        nums.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    
+    enum num : String {
+        case n2 = "2"
+        case n3 = "3"
+        case n4 = "4"
+    }
+    
+    @IBAction func numIsTapped(_ sender: UIButton) {
+        guard let title = sender.currentTitle, let n = num(rawValue: title) else {
+            return
+        }
+        nums.forEach { (button) in
+                   UIView.animate(withDuration: 0.3, animations: {
+                       button.isHidden = !button.isHidden
+                       self.view.layoutIfNeeded()
+                   })
+               }
+        if (n == .n2 ){
+            selectButton.setTitle("2", for: .normal)
+            self.n = 2
+        } else if(n == .n3){
+            selectButton.setTitle("3", for: .normal)
+            self.n = 3
+        } else {
+            selectButton.setTitle("4", for: .normal)
+            self.n = 4
+        }
+    }
+    @IBAction func checkBoxTapped(_ sender: UIButton) {
+        if sender.isSelected {
+            sender.isSelected = false
+            check -= 1
+        } else{
+            if(check < n){
+                sender.isSelected = true
+                check += 1
+            }
+            
+        }
+    }
+    
+    }
+
+
+class ValidateViewController : UIViewController{
+override func viewDidLoad() {
+    
+        super.viewDidLoad()
+    }
+}
+
+class ShakeViewController : UIViewController{
+override func viewDidLoad() {
+    
+        super.viewDidLoad()
+    }
+}
