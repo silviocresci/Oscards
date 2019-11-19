484a2a3b05ac03a5b525cb6ab7dcf975616056bb
Silvio <silviocresci1996@gmail.com>
2019-11-19 09:29:55 +0100

    Other views

--- Oscards/ViewController.swift
+++ Oscards/ViewController.swift
@@ -30,7 +30,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var textArea1: UITextView!
     @IBOutlet weak var textArea2: UITextView!
     
-   
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    
+    @IBAction func selectNumber(_ sender: UIButton) {
+        button1.setTitleColor(.black, for: .normal)
+        button2.setTitleColor(.black, for: .normal)
+        button3.setTitleColor(.black, for: .normal)
+        button4.setTitleColor(.black, for: .normal)
+        
+        sender.setTitleColor(.green, for: .normal)
+
+    }
+    
+    
 }
 class SecondViewController : UIViewController{
     override func viewDidLoad() {
@@ -44,7 +59,34 @@ class RulesViewController : UIViewController{
 override func viewDidLoad() {
     
         super.viewDidLoad()
+    let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
+    rightSwipe.direction = .right
+    let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
+    leftSwipe.direction = .left
+    
+    view.addGestureRecognizer(rightSwipe)
+    view.addGestureRecognizer(leftSwipe)
+
     }
+    
+    @IBOutlet weak var textArea: UITextView!
+    @IBOutlet weak var dots: UIPageControl!
+    @objc func handleSwipe(sender : UISwipeGestureRecognizer){
+        if sender.state == .ended{
+            switch sender.direction {
+            case .right:
+                textArea.text = "Rules Multiplayer rules multiplayer rules multiplayer Rules Multiplayer rules multiplayer rules multiplayer Rules Multiplayer rules multiplayer rules multiplayer Rules Multiplayer rules multiplayer rules multiplayer Rules Multiplayer rules multiplayer rules multiplayer Rules Multiplayer rules multiplayer rules multiplayer Rules Multiplayer rules multiplayer rules multiplayer Rules Multiplayer rules multiplayer rules multiplayer Rules Multiplayer rules multiplayer rules multiplayer Rules Multiplayer rules multiplayer rules multiplayer  "
+                dots.currentPage = 0
+            case .left:
+                               textArea.text = "Rules Singleplayer rules singleplayer rules singleplayer Rules Singleplayer rules singleplayer rules singleplayer Rules Singleplayer rules singleplayer rules singleplayer Rules Singleplayer rules singleplayer rules singleplayer Rules Singleplayer rules singleplayer rules singleplayer Rules Singleplayer rules singleplayer rules singleplayer Rules Singleplayer rules singleplayer rules singleplayer Rules Singleplayer rules singleplayer rules singleplayer Rules Singleplayer rules singleplayer rules singleplayer Rules Singleplayer rules singleplayer rules singleplayer  "
+                               dots.currentPage = 1
+            default:
+                break
+            }
+        }
+    }
+    
+    
 }
 
 class HomeViewController : UIViewController{
@@ -58,6 +100,7 @@ class SelectViewController : UIViewController{
 
     @IBOutlet weak var selectButton: UIButton!
     @IBOutlet var nums: [UIButton]!
+    @IBOutlet weak var playButton: UIButton!
     
     var n : CGFloat = 0
     var check : CGFloat = 0
@@ -65,6 +108,7 @@ class SelectViewController : UIViewController{
     override func viewDidLoad() {
     
         super.viewDidLoad()
+        enableButton()
     }
     @IBAction func handleSelection(_ sender: UIButton) {
         nums.forEach { (button) in
@@ -94,22 +138,39 @@ class SelectViewController : UIViewController{
         if (n == .n2 ){
             selectButton.setTitle("2", for: .normal)
             self.n = 2
+            enableButton()
         } else if(n == .n3){
             selectButton.setTitle("3", for: .normal)
             self.n = 3
+            enableButton()
         } else {
             selectButton.setTitle("4", for: .normal)
             self.n = 4
+            enableButton()
         }
     }
+    func enableButton(){
+        
+        if (n != check || check == 0){
+            playButton.isEnabled = false
+            playButton.layer.opacity = 0.5
+              }else{
+            playButton.isEnabled = true
+            playButton.layer.opacity = 1
+
+              }
+    }
+    
     @IBAction func checkBoxTapped(_ sender: UIButton) {
         if sender.isSelected {
             sender.isSelected = false
             check -= 1
+            enableButton()
         } else{
             if(check < n){
                 sender.isSelected = true
                 check += 1
+                enableButton()
             }
             
         }
@@ -128,6 +189,12 @@ override func viewDidLoad() {
 class ShakeViewController : UIViewController{
 override func viewDidLoad() {
     
+        super.viewDidLoad()
+    }
+}
+
+class SinglePlayerViewController : UIViewController {
+    override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
